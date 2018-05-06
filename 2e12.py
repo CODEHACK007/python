ff84e21a893285b5b6682d3862d3dddf31bb10d6
CODEHACK007 <37002938+CODEHACK007@users.noreply.github.com>
2018-05-06 23:09:27 +0530

    Update 2e12.py

--- 2e12.py
+++ 2e12.py
@@ -7,7 +7,7 @@ for i in range(0,10):
 for i in range(0,10,1):
     print(i)
     
-#all three give same output
+#3 programs above give same output
 Python 3.6.1 (default, Dec 2015, 13:05:11)
 [GCC 4.8.2] on linux
    
