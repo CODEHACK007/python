278f1bb1d2122b149f0ae3d69d65fa0c7fe42e08
CODEHACK007 <37002938+CODEHACK007@users.noreply.github.com>
2018-05-06 23:09:01 +0530

    Create 2e13.py

---
+++ 2e13.py
@@ -0,0 +1,22 @@
+for i in range(1,11):
+    print(i)
+    
+i = 1
+while i < 11:
+    print(i)
+    i = i+1
+
+#2 programs above give same output
+Python 3.6.1 (default, Dec 2015, 13:05:11)
+[GCC 4.8.2] on linux
+   
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10   
