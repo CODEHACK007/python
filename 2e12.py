ff6f6611b29c18417690314b6f9ad4d1029ed219
CODEHACK007 <37002938+CODEHACK007@users.noreply.github.com>
2018-05-06 23:00:36 +0530

    Create 2e12.py

---
+++ 2e12.py
@@ -0,0 +1,23 @@
+for i in range(10):
+    print(i)
+
+for i in range(0,10):
+    print(i)
+    
+for i in range(0,10,1):
+    print(i)
+    
+#all three give same output
+Python 3.6.1 (default, Dec 2015, 13:05:11)
+[GCC 4.8.2] on linux
+   
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
